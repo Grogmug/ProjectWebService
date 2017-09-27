ec04558e09ead8e637265922d9bf7f7a4216967b
Søren <Sren@DESKTOP-3DFGV3A>
2017-09-27 12:10:05 +0200

    Add project files.

---
+++ SensorClient/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorClient
+{
+    class Program
+    {
+        string line;
+        static void Main(string[] args)
+        {
+            Program prog = new Program();
+            prog.CollectSensorData();
+        }
+
+        public void Run()
+        {
+
+        }
+
+        public void CollectSensorData()
+        {
+            System.IO.StreamReader file = new System.IO.StreamReader("logfil.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                string[] dataSeperated = SeperateString(line);
+
+                Console.WriteLine($"Tid: {dataSeperated[0]} Id: {dataSeperated[1]} Alarm: {dataSeperated[2]} Navn: {dataSeperated[3]} Afdeling: {dataSeperated[4]} Bolig: {dataSeperated[5]} Afmeldt: {dataSeperated[7]}");
+            }
+            file.Close();
+            Console.ReadLine();
+        }
+        public string[] SeperateString(string input)
+        {
+            return input.Split('\t');
+        }
+    }
+}
